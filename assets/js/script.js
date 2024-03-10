9e4f2f7897e7e6f82206d3782723ff39bdacb9b2
Marco Toppino <marco.toppino@gmail.com>
2024-03-10 19:46:35 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame() {
+
+}
+
+function chackAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incremenetScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+    
+}
+
